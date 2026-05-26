d18cba1b51c971a088647998220a2cb18ff84560
daveod06 <davidodonnell88@gmail.com>
2026-05-26 09:04:29 -0600

    fix some accidentally commented code and remove unnecessary dependencies

--- Scripts/Game/JWK/FreedomFighters/Components/Military/BM_JWK_BattleSubjectComponent.c
+++ Scripts/Game/JWK/FreedomFighters/Components/Military/BM_JWK_BattleSubjectComponent.c
@@ -1,22 +1,3 @@
-/**********************************************************************************************************************
- ************************************************* FREEDOM FIGHTERS ***************************************************
- **********************************************************************************************************************
- * This file is part of the Freedom Fighters project, published under modified APL-ND license. You are free to adapt  *
- * (i.e. modify, rework or update) and share (i.e. copy, distribute or transmit) the material under the following     *
- * conditions:                                                                                                        *
- * - Attribution - You must attribute the material in the manner specified by the author or licensor (but not in any  *
- *   way that suggests that they endorse you or your use of the material).                                            *
- * - Noncommercial - You may not use this material for any commercial purposes.                                       *
- * - Arma Only - You may not convert or adapt this material to be used in other games than Arma.                      *
- * - No Derivatives - If you remix, transform, or build upon the material, you may not distribute the modified        *
- *   material.                                                                                                        *
- * The above list is only a highlight and is NOT an exhaustive list of restrictions. Please visit the following URL   *
- * to view the full text of the license:                                                                              *
- *    https://www.johnnykerner.dev/FreedomFighters/project-license/20250916/                                          *
- **********************************************************************************************************************
- * For more info or to contact the author, visit the project website:                                                 *
- *    https://www.johnnykerner.dev/FreedomFighters/                                                                   *
- ******************************************************************************************************************** */
 // BakerMods overrides applied:
 //   Fix 2:  DoBattleFinish_S captures location for invaders when they win.
 //   Fix 3:  Garrison groups tracked in m_BMGarrisonForce; cleaned up on battle finish.
